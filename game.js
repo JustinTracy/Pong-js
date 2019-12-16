9be00e8ba6ef6c2e6de08146bde0bcdcda633f3f
Justin <Tracyjn@mail.uc.edu>
2019-12-15 19:56:23 -0500

    Ball now bounces off of walls

--- game.js
+++ game.js
@@ -5,12 +5,17 @@ const canvasWidth = 800;
 const canvasHeight = 550;
 const ballSize = 25;
 
+let xLocation = canvasWidth / 2;
+let yLocation = canvasHeight / 2;
+let xSpeed = 10;
+let ySpeed = xSpeed;
+
 function InitializeGame()
 {
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
     context.fillStyle = 'white';
-    context.fillRect(canvasWidth/2 - ballSize, canvasHeight/2 - ballSize, ballSize, ballSize);
+    context.fillRect(xLocation, yLocation, ballSize, ballSize);
     context.font = '48px arial';
     context.fillText('0', 10, 50, 500, 500);
     context.fillText('0', canvasWidth - 40, 50, 500, 500);
@@ -19,4 +24,14 @@ function InitializeGame()
 
 function moveBall()
 {
+    context.clearRect(xLocation, yLocation, ballSize, ballSize);
+
+    if (xLocation + ballSize >= canvasWidth) xSpeed = -xSpeed;
+    if (xLocation <= 0) xSpeed = -xSpeed;
+    if (yLocation + ballSize >= canvasHeight) ySpeed = -ySpeed;
+    if (yLocation <= 0) ySpeed = -ySpeed;
+    xLocation += xSpeed;
+    yLocation -= ySpeed;
+    
+    context.fillRect(xLocation, yLocation, ballSize, ballSize);
 }
